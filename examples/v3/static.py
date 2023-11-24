4e67a28c831737789348df4f605762cb8b57899d
Joeri Hermans <joeri@peinser.com>
2023-11-24 21:10:17 +0100

    Fix linting issues in the examples

--- examples/v3/static.py
+++ examples/v3/static.py
@@ -9,16 +9,19 @@ from __future__ import annotations
 
 import asyncio
 
+import pyconiq.qr  # type: ignore[import-untyped]
 import uvloop
-
-import pyconiq.qr
 
 
 async def main() -> None:
+    # First, we need a QR code that is associated with our PoS (point of sale).
+    # In this case, the identifier of the PoS is `test`.
     qr = pyconiq.qr.static(pos="test")
+    # Show the QR code in the terminal.
+    qr.print_ascii(tty=True)
 
-    img = qr.make_image()
-    img.save("pos.png")
+    # Initiate a payment request.
+    # TODO Implement
 
 
 with asyncio.Runner(loop_factory=uvloop.new_event_loop) as runner:
